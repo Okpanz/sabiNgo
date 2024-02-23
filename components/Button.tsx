48177ce9fec80ce0dff11d02053f8084bc491bbf
Okpanz <okpanz700@gmail.com>
2024-02-23 14:37:22 +0100

    literal errors solved

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,14 +3,12 @@ import { clsx } from "clsx";
 import ArrowIconleft from "./icon/ArrowIconleft";
 
 const btnColor: Record<string, string> = {
-  primary: "  bg-primary  text-white  hover:bg-white  hover:text-primary  ",
-  secondry:
-    "  bg-white text-primary     hover:bg-primary  hover:text-white    ",
+  primary: "bg-primary text-white hover:bg-white hover:text-primary",
+  secondry: "bg-white text-primary hover:bg-primary hover:text-white",
 };
 
 function Button({
   label,
-
   icon,
   type,
   classes = "",
@@ -19,8 +17,7 @@ function Button({
   disabled = false,
 }: {
   label?: string;
-
-  type: "button" | "submite" | "reset";
+  type: "button" | "submit" | "reset"; 
   color?: "primary" | "secondry";
   icon?: React.ReactNode;
   classes?: string;
@@ -31,19 +28,18 @@ function Button({
   return (
     <button
       type={type}
-      disable={disabled}
+      disabled={disabled} // Corrected "disable" to "disabled"
       className={clsx(
-        "  flex   font-montse font-[500]   gap-1 py-[12px] px-[17px]  lg:text-[16px]  md:text-[12px] text-[9px] rounded-lg  whitespace-nowrap   items-center wrap border-primary border-2  white  ",
+        "flex font-montse font-[500] gap-1 py-[12px] px-[17px] lg:text-[16px] md:text-[12px] text-[9px] rounded-lg whitespace-nowrap items-center wrap border-primary border-2 white",
         classes || "",
         disabled ? "opacity-40 hover:!opacity-40 pointer-events-none" : "",
-        btnColor[`${color}`] || ""
+        btnColor[color] || ""
       )}
       onClick={onClick}
     >
-      <div> {label}</div>
-
-      <span> {icon} </span>
-      <ArrowIconleft className="  lg:h-4 lg:w-4    h-3 w-3" color="white" />
+      <div>{label}</div>
+      <span>{icon}</span>
+      <ArrowIconleft className="lg:h-4 lg:w-4 h-3 w-3" color="white" />
     </button>
   );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,60 +8,48 @@ import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa6"
 
 function Footer() {
   return (
-    <div
-      className="bg-[#2A2A2A] text-[18px] font-[500] font-body text-white
-  "
-    >
+    <div className="bg-[#2A2A2A] text-[18px] font-[500] font-body text-white">
       <Container>
-        <div className="flex flex-col md:flex-row mx-auto justify-center my-24  gap-20 w-[80%] py-11">
-          <div className=" w-[280px]">
-            <Image src={Logo} alt="Logo" />{" "}
-            <p className=" leading-7 mt-4">
-              Lorem Ipsum is simply dummy text of the industry's since the
-              unknown. Lorem Ipsum is simply dummy text of the industry.
+        <div className="flex flex-col md:flex-row mx-auto justify-center my-24 gap-20 w-[80%] py-11">
+          <div className="w-[280px]">
+            <Image src={Logo} alt="Logo" />
+            <p className="leading-7 mt-4">
+              Lorem Ipsum is simply dummy text of the industry's since the unknown. Lorem Ipsum is simply dummy text of the industry.
             </p>
           </div>
           <div>
-            <h1 className="text-[24px] font-[500] ">Gallery</h1>
-            <Image className=" mt-4" src={Gallary} alt="Gallery" />
+            <h1 className="text-[24px] font-[500]">Gallery</h1>
+            <Image className="mt-4" src={Gallary} alt="Gallery" />
           </div>
           <div>
             <h1 className="text-[24px] font-[500] mb-4">Quick Link</h1>
-
             <ul>
-              <li className=" mb-4">
-                <Link href="/">About Us </Link>
-              </li>
-
               <li className="mb-4">
-                <Link href="/">Leadership </Link>
+                <Link href="/">About Us</Link>
               </li>
-
               <li className="mb-4">
-                <Link href="/">FAQ’s </Link>
+                <Link href="/">Leadership</Link>
               </li>
-
               <li className="mb-4">
-                <Link href="/">Become Volunteer </Link>
+                <Link href="/">FAQ’s</Link>
+              </li>
+              <li className="mb-4">
+                <Link href="/">Become Volunteer</Link>
               </li>
             </ul>
           </div>
         </div>
       </Container>
-
-      <div className="bg-white h-2 "></div>
+      <div className="bg-white h-2"></div>
       <Container>
-        <div className=" flex  justify-between py-5">
+        <div className="flex justify-between py-5">
           <div>
-            {" "}
-            © All right reserved 2024{" "}
-            <span className="text-primary"> Sabi NGO </span>{" "}
+            © All right reserved 2024 <span className="text-primary">Sabi NGO</span>
           </div>
-
-          <div className=" flex gap-4">
-            <FaFacebook  className="hover:text-primary cursor-pointer transition-all ease-in-out duration-300" />
-            <FaLinkedin  className="hover:text-primary cursor-pointer transition-all ease-in-out duration-300"/>
-            <FaInstagram  className="hover:text-primary cursor-pointer transition-all ease-in-out duration-300"/>
+          <div className="flex gap-4">
+            <FaFacebook className="hover:text-primary cursor-pointer transition-all ease-in-out duration-300" />
+            <FaLinkedin className="hover:text-primary cursor-pointer transition-all ease-in-out duration-300" />
+            <FaInstagram className="hover:text-primary cursor-pointer transition-all ease-in-out duration-300" />
             <FaTwitter className="hover:text-primary cursor-pointer transition-all ease-in-out duration-300" />
           </div>
         </div>
